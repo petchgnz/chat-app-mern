e97cde5f2687de1fb17dd9cdd02477fddfe872b7
petchgnz <phetchgnz@gmail.com>
2025-11-03 14:57:33 +0700

    Project Completed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: import.meta.env.MODE === "developement" ? "http://localhost:3001/api" : "/api",
   withCredentials: true
 })
